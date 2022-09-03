9ec80b217405481532cb7658f2954dc3049ae838
Amit Shwarts <47475100+AmitShwarts@users.noreply.github.com>
2022-09-03 15:49:52 +0300

    Feat/improve mobile app error handling (#12)
    * feat: improve mobile app error handling

--- mobile-app/src/app/state/sensors/serverAPI.ts
+++ mobile-app/src/app/state/sensors/serverAPI.ts
@@ -4,11 +4,19 @@ import {getUrl} from '../../../constants/serverConfigs';
 const getSensorsListEndpoint = 'getSensors';
 
 const getSensors = async (): Promise<Sensor[]> => {
-  const data = await fetchService.fetch(getUrl(getSensorsListEndpoint), {
-    method: 'GET',
-  });
+  let data = [];
 
-  return data.sensorsData as Sensor[];
+  try {
+    data = await fetchService.fetch(getUrl(getSensorsListEndpoint), {
+      method: 'GET',
+    });
+
+    data = data.sensorsData;
+  } catch (err: any) {
+    console.warn(err.message);
+  } finally {
+    return data as Sensor[];
+  }
 };
 
 export const serverAPI = {
