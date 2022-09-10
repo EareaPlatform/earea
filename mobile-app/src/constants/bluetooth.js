955bb7bb07755abfd93185294f7e126f2dc83601
AmitShwarts <amit.shwarts@gmail.com>
2022-09-10 12:22:40 +0300

    remove

--- mobile-app/src/constants/bluetooth.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.bluetoothDeviceAddress = void 0;
-exports.bluetoothDeviceAddress = 'FE:DC:40:4A:38:33';
--- mobile-app/src/constants/environment.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.isDevMode = void 0;
-exports.isDevMode = process.env.NODE_ENV === 'development';
